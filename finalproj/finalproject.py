1837899c4d45aa7751ce38ac73be601289838a32
Maggie Wang <margaret.r.wang@hotmail.com>
2019-01-03 15:24:21 +0000

    intro fixed again lolz

--- finalproj/finalproject.py
+++ finalproj/finalproject.py
@@ -1,7 +1,8 @@
-import random
 import time
 
-print ( "welcome, bienvenido, bem vinda, 欢迎, ようこそ, to Maggie's final project")
+##intro
+
+print ( "welcome, bienvenido, benvenuto, bem vinda, 欢迎, ようこそ, to Maggie's final project")
 time.sleep(1)
 print('you will embark on the ultimate adventure around LA')
 time.sleep(2)
@@ -14,7 +15,7 @@ time.sleep(2)
 print( "and here is a little bit of good luck from me cuz you'll need it")
 time.sleep(2)
 
-start = input( 'are you ready to begin the adventure?!')
+start = input( 'are you ready to begin the adventure?! ')
 
 if start == 'yes' or 'y' or 'sure' or 'yeah' or 'ye' or 'yeet':
     print("of course you are. I expected no less of you. let's gooo")
@@ -22,3 +23,9 @@ else:
     print("welp. too bad. we are starting anyway")
 
 time.sleep(1)
+for x in range (0,5):
+    b = "Loading" + "." * x
+    print (b, end="\r")
+    time.sleep(1)
+
+## final battle setup
