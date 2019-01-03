6a3dfd8f3114def3a1426aa815bdbd35ad0508a9
Maggie Wang <margaret.r.wang@hotmail.com>
2019-01-03 15:06:21 +0000

    fixed intro

--- finalproj/finalproject.py
+++ finalproj/finalproject.py
@@ -4,19 +4,21 @@ import time
 print ( "welcome, bienvenido, bem vinda, 欢迎, ようこそ, to Maggie's final project")
 time.sleep(1)
 print('you will embark on the ultimate adventure around LA')
+time.sleep(2)
+print( 'you will be exploring important landmarks - and by that I mean the houses of hot celebrities and youtubers')
 time.sleep(1)
-print( 'you will be exploring important landmarks - and by that I mean the houses of hot celebrities and youtubers - and collecting allies, items, and more along the way')
+print( 'and collecting allies, items, and more along the way')
 time.sleep(3)
 print( 'this is all in preparation for an ePiC battle at the end')
-time.sleep(1)
+time.sleep(2)
 print( "and here is a little bit of good luck from me cuz you'll need it")
-time.sleep(1)
+time.sleep(2)
 
 start = input( 'are you ready to begin the adventure?!')
 
 if start == 'yes' or 'y' or 'sure' or 'yeah' or 'ye' or 'yeet':
-    print("of course you are. I expected no less of you. let's gooo')
-elif:
+    print("of course you are. I expected no less of you. let's gooo")
+else:
     print("welp. too bad. we are starting anyway")
 
 time.sleep(1)
